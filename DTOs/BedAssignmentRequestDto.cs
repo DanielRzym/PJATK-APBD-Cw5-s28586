c530201d6b0804428a3ecf4336e3b8c5e68b4157
Daniel <drzymanski@gmail.com>
2026-06-12 13:50:48 +0200

    Zadanie 3: Endpoint POST z dostosowanym DTO

---
+++ DTOs/BedAssignmentRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PJATK_APBD_Cw5_s28586.DTOs
+{
+    public class BedAssignmentRequestDto
+    {
+        public DateTime From { get; set; }
+        public DateTime? To { get; set; }
+        public string BedType { get; set; } = string.Empty;
+        public string Ward { get; set; } = string.Empty;
+    }
+}
