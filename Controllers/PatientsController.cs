72bda959ec605bfbc6f08712a4c250379feecb9d
Daniel <drzymanski@gmail.com>
2026-06-12 13:50:48 +0200

    Zadanie 3: Endpoint POST z dostosowanym DTO

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PJATK_APBD_Cw5_s28586.Models;
-namespace PJATK_APBD_Cw5_s28586.Controllers
+using PJATK_APBD_Cw5_s28586.DTOs; 
 
+namespace PJATK_APBD_Cw5_s28586.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -28,14 +29,12 @@ namespace PJATK_APBD_Cw5_s28586.Controllers
                     EF.Functions.Like(p.LastName, searchTerm));
             }
 
-           
             var patients = await query.Select(p => new
             {
                 pesel = p.Pesel,
                 firstName = p.FirstName,
                 lastName = p.LastName,
                 age = p.Age,
-                
                 sex = p.Sex == true ? "Male" : "Female", 
                 
                 admissions = p.Admissions.Select(a => new
@@ -81,6 +80,49 @@ namespace PJATK_APBD_Cw5_s28586.Controllers
             }).ToListAsync();
 
             return Ok(patients);
+        }
+
+        [HttpPost("{pesel}/bedassignments")]
+        public async Task<IActionResult> AddBedAssignment(string pesel, [FromBody] BedAssignmentRequestDto request)
+        {
+            
+            var patientExists = await _context.Patients.AnyAsync(p => p.Pesel == pesel);
+            if (!patientExists)
+            {
+                return NotFound("Pacjent o podanym numerze PESEL nie istnieje w bazie.");
+            }
+
+          
+            var availableBed = await _context.Beds
+                .Where(b => b.Room.Ward.Name == request.Ward && b.BedType.Name == request.BedType)
+                .Where(b => !b.BedAssignments.Any(ba =>
+                  
+                    (request.To == null || ba.From < request.To) && 
+                    (ba.To == null || ba.To > request.From)
+                ))
+                .FirstOrDefaultAsync();
+
+            if (availableBed == null)
+            {
+                return NotFound("Brak wolnych łóżek wybranego typu na tym oddziale we wskazanym terminie.");
+            }
+
+          
+            var newAssignment = new BedAssignment
+            {
+                PatientPesel = pesel,
+                BedId = availableBed.Id, 
+                From = request.From,
+                To = request.To
+            };
+
+            _context.BedAssignments.Add(newAssignment);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created, new { 
+                Message = "Pomyślnie przypisano łóżko.", 
+                AssignmentId = newAssignment.Id 
+            });
         }
     }
 }
